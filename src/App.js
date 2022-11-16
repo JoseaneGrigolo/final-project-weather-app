f48075f57b82ee7cfa52e4417c547e4bc1cb4c63
Joseane I Grigolo <joseaneig@gmail.com>
2022-11-16 09:47:42 -0600

    updade project

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/joseane-ivelise-grigolo-863a1a35/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Joseane Grigolo{" "}
+          </a>
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/JoseaneGrigolo/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub{" "}
+          </a>
+          and hosted on{" "}
+          <a
+            href="https://github.com/JoseaneGrigolo/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Netlify
+          </a>
+        </footer>{" "}
+      </div>
+    </div>
+  );
+}
+
+export default App;
